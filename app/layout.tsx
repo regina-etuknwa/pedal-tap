c67b4096a42f04efb92a7cddd9bbe3797f4366eb
reginanyong <104439305+regina-etuknwa@users.noreply.github.com>
2025-01-22 13:00:47 +0100

    adjusted padding of main

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,13 @@ export default function RootLayout({
       >
         <ThemeRegistry>
           <NavBar />
-          <Box component="main" >
-            <Toolbar />
+          <Box component="main"  >
+            <Toolbar 
+              sx={{height: {
+                xs: '100px', // Smaller width on xs screens
+                md: '120px', // Default size on large screens
+                },
+              }} />
             {children}
           </Box>
         </ThemeRegistry>
